7d13e279bbf9cba999fbc25eca27ad0dc57b75fa
ericadebem <ericadebemm@gmail.com>
2022-11-01 19:27:07 +0100

    test

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello word");
+console.log("Mandando oi pro amigo!");
+
+function mandaMensagem() {
+    console.log("Tudo bem?");
+    console.log("Vou te mandar uma solicitação!");
+    console.log("Solicitação recebida!");
+}
+
+setTimeout(mandaMensagem, 5000);
+
+console.log("tchau!")
