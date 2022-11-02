b3a6cc3f45f2ae1b7c74bf091829f2b0040d9630
ericadebem <ericadebemm@gmail.com>
2022-11-02 21:17:56 +0100

    testing then catch and finally

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
 .then(resposta => resposta.json())
-.then(r => console.log(r));
+.then(r => {
+    if(r.erro){
+        throw Error('Esse cep não existe!')
+    }else
+    console.log(r)})
+.catch(erro => console.log(erro))
+.finally(mensagem => console.log('Processamento concluido'));
     
 console.log(consultaCEP);
