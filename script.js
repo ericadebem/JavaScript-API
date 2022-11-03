a1d1c90cf726d889ec7c57b07a85831ab87c1e5f
ERICA DE BEM <110907550+ericadebem@users.noreply.github.com>
2022-11-03 17:35:51 +0100

    add async await

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-let consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-.then(resposta => resposta.json())
-.then(r => {
-    if(r.erro){
-        throw Error('Esse cep não existe!')
-    }else
-    console.log(r)})
-.catch(erro => console.log(erro))
-.finally(mensagem => console.log('Processamento concluido'));
+async function buscaEndereco() {
+    let consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/')
     
-console.log(consultaCEP);
+    console.log(consultaCEP);
+    
+    }
+    
+    buscaEndereco();
