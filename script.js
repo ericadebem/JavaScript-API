ef4711f23e8038d856e538bd09bdca875dd899c2
ericadebem <ericadebemm@gmail.com>
2022-11-05 20:55:54 +0100

    try and catch

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 async function buscaEndereco() {
+    try {
     let consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/')
     let consultaCEPConvertida = await consultaCEP.json()
     console.log(consultaCEPConvertida);
-    }
+ } catch(erro){}
+ } 
     
     buscaEndereco();
