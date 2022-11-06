1236d4c5b247c8e63a96cbe4804596208e4b869b
ericadebem <ericadebemm@gmail.com>
2022-11-06 18:57:49 +0100

    add array cep

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-async function buscaEndereco() {
-    try {
-    let consultaCEP = await fetch('https://viacep.com.br/ws/01001250/json/')
-    let consultaCEPConvertida = await consultaCEP.json()
-    if (consultaCEPConvertida.erro){
+async function buscaEndereco(cep) {
+   try {
+     let consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+     let consultaCEPConvertida = await consultaCEP.json()
+     if (consultaCEPConvertida.erro){
       throw Error('CEP não existente!');
     }
     console.log(consultaCEPConvertida);
  } catch(erro){
     console.log(erro)
-     }
+   }
  } 
     
     buscaEndereco();
